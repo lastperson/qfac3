377195802daca21ac423898f1099b3daa9b307f7
allchick <allchick@ua.fm>
2014-07-28 20:59:45 +0300

    Added new code :)

--- allchick/My project/src/com/company/Strahovator.java
+++ allchick/My project/src/com/company/Strahovator.java
@@ -14,7 +14,7 @@ public class Strahovator {
 
     public static void main(String[] args){
 
-        System.setProperty("webdriver.chrome.driver", "D:\\Tools\\chromedriver.exe");
+        /*System.setProperty("webdriver.chrome.driver", "D:\\Tools\\chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
 
         webDriver.get("http://strahovator.com.ua/");
@@ -32,16 +32,89 @@ public class Strahovator {
         Select tsZone = new Select(webDriver.findElement(By.xpath("//select[@id='ts_zone']")));
         tsZone.selectByVisibleText("Борисполь");
 
-        Select noCrashYearsQty = new Select(webDriver.findElement(By.xpath("no_crash_years_qty")));
+        Select noCrashYearsQty = new Select(webDriver.findElement(By.xpath("//select[@id = 'no_crash_years_qty']")));
         noCrashYearsQty.selectByVisibleText("6 лет и более");
 
-        Select period = new Select(webDriver.findElement(By.xpath("period")));
+        Select period = new Select(webDriver.findElement(By.xpath("//select[@id = 'period']")));
         period.selectByVisibleText("8 месяцев");
 
-        Select dgo = new Select(webDriver.findElement(By.xpath("dgo")));
+        Select dgo = new Select(webDriver.findElement(By.xpath("//select[@id = 'dgo']")));
         dgo.selectByVisibleText("нет");
 
-        //webDriver.quit();
+        String firstPrice = webDriver.findElement(
+                By.xpath("//table[@class='osago_result']/tbody/tr[1]/td[@class = 'center']/span[@class = 'price']"))
+                .getText().trim();
+        System.out.println(firstPrice);
 
+        String secondPrice = webDriver.findElement(
+                By.xpath("//table[@class='osago_result']/tbody/tr[2]/td[@class = 'center']/span[@class = 'price']"))
+                .getText().trim();
+        System.out.println(secondPrice);
+
+
+        webDriver.findElement(By.xpath("//table/tbody/tr[2]/td[3]/button[@type='submit']")).click();
+        while (webDriver.findElements(By.id("main")).size() == 0);
+
+        webDriver.findElement(By.xpath("//a/strong[contains(text(), 'без регистрации')]/..")).click();
+        while (webDriver.findElements(By.xpath("//div[@class = 'service osago-service']")).size() == 0);
+
+        webDriver.findElement(By.xpath("//a[@role = 'button' and contains(text(), 'Купить на 20 гривен дешевле')]")).click();
+        while (webDriver.findElements(By.xpath("//div[@class = 'row-fluid']")).size() == 0);
+
+        webDriver.findElement(By.xpath("//label [@class='radio']/input[not(@checked)]")).click();
+        String placeholder1 = webDriver.findElement(By.xpath("//input [@id = 'policy_last_name']")).getAttribute("placeholder").toString();
+        String placeholder2 = webDriver.findElement(By.xpath("//input [@id = 'policy_first_name']")).getAttribute("placeholder").toString();
+        String placeholder3 = webDriver.findElement(By.xpath("//input [@id = 'policy_parent_name']")).getAttribute("placeholder").toString();
+
+        System.out.println(placeholder1);
+        System.out.println(placeholder2);
+        System.out.println(placeholder3);
+
+        webDriver.findElement(By.xpath("//a[contains(text(), 'Главная')]")).click();
+        while (webDriver.findElements(By.xpath("//div[@class = 'content']")).size() == 0);*/
+
+        /*String text1 = webDriver.findElement(By.xpath("//i[@class= 'icon-file-text']/../a")).getText().toString();
+        String text2 = webDriver.findElement(By.xpath("//i[@class= 'icon-leaf']/../a")).getText().toString();
+        String text3 = webDriver.findElement(By.xpath("//i[@class= 'icon-shield']/../a")).getText().toString();
+        String text4 = webDriver.findElement(By.xpath("//i[@class= 'icon-globe']/../a")).getText().toString();
+        System.out.println(text1);
+        System.out.println(text2);
+        System.out.println(text3);
+        System.out.println(text4);*/
+
+        System.out.println(returnValue(3));
+
+        //webDriver.quit();
+    }
+
+    public static String returnValue(int value){
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Tools\\chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.get("http://strahovator.com.ua/");
+        while (webDriver.findElements(By.xpath("//a[text()='ОСАГО']")).size() == 0);
+
+        String valueForReturn;
+
+        switch (value){
+            case 1:
+                valueForReturn = webDriver.findElement(By.xpath("//i[@class= 'icon-file-text']/../a")).getText().toString();
+                break;
+            case 2:
+                valueForReturn = webDriver.findElement(By.xpath("//i[@class= 'icon-leaf']/../a")).getText().toString();
+                break;
+            case 3:
+                valueForReturn = webDriver.findElement(By.xpath("//i[@class= 'icon-shield']/../a")).getText().toString();
+                break;
+            case 4:
+                valueForReturn = webDriver.findElement(By.xpath("//i[@class= 'icon-globe']/../a")).getText().toString();
+                break;
+            default:
+                valueForReturn = "Incorrect input value";
+                break;
+        }
+
+        return valueForReturn;
     }
 }
