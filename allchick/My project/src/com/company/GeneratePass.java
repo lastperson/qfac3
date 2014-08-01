1762fddc6b456e76b4d6f1d19126dc6e8b7fead8
allchick <allchick@ua.fm>
2014-08-01 20:58:03 +0300

    A lot of new functions

--- allchick/My project/src/com/company/GeneratePass.java
+++ allchick/My project/src/com/company/GeneratePass.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,21 +11,83 @@ import org.openqa.selenium.chrome.ChromeDriver;
  */
 public class GeneratePass {
 
-
-
-    public static String GeneratePassword (String password, String site){
-
+    public static void OpenGeneratePasswordPage(){
         ChromeWebDriver.OpenSite("http://angel.net/~nic/passwd.current.html");
+        while (ChromeWebDriver.webDriver.findElements(By.className("callout")).size() == 0);
+    }
 
+    public static WebElement MasterPasswordField(){
         WebElement masterPasswordField = ChromeWebDriver.webDriver.findElement(By.xpath("//input[@type='password']"));
-        masterPasswordField.sendKeys(password);
-        WebElement siteField = ChromeWebDriver.webDriver.findElement(By.xpath("//input[@autocorrect='off']"));
-        siteField.sendKeys(site);
-        WebElement generateKey = ChromeWebDriver.webDriver.findElement(By.xpath("//input[@type='submit']"));
-        generateKey.click();
-        WebElement passwordField = ChromeWebDriver.webDriver.findElement(By.xpath("//input[@name='password']"));
-        String generatedPassword = passwordField.getAttribute("value");
+        return masterPasswordField;
+    }
 
+    public static WebElement SiteName(){
+        WebElement siteName = ChromeWebDriver.webDriver.findElement(By.xpath("//input[@autocorrect='off']"));
+        return siteName;
+    }
+
+    public static WebElement GenerateButton(){
+        WebElement generateButton = ChromeWebDriver.webDriver.findElement(By.xpath("//input[@type='submit']"));
+        return generateButton;
+    }
+
+    public static WebElement GeneratedPassword(){
+        WebElement generatedPassword = ChromeWebDriver.webDriver.findElement(By.xpath("//input[@name='password']"));
+        return generatedPassword;
+    }
+
+    public static void EnterMasterPassword(String pass){
+        GeneratePass.MasterPasswordField().sendKeys(pass);
+    }
+
+    public static String GetMasterPassword(){
+        String masterPassword = GeneratePass.MasterPasswordField().getAttribute("value");
+        return masterPassword;
+    }
+
+    public static void EnterSiteName(String site){
+        GeneratePass.SiteName().sendKeys(site);
+    }
+
+    public static String GetSiteName(){
+        String siteName = GeneratePass.SiteName().getAttribute("value");
+        return siteName;
+    }
+
+    public static void ClickGenerateButton(){
+        GeneratePass.GenerateButton().click();
+    }
+
+    public static String GetGeneratedPassword(){
+        String generatedPassword = GeneratePass.GeneratedPassword().getAttribute("value");
         return  generatedPassword;
+    }
+
+    public static void ControlsAreExist(){
+        Assert.assertTrue(GeneratePass.MasterPasswordField().isDisplayed());
+        Assert.assertTrue(GeneratePass.SiteName().isDisplayed());
+        Assert.assertTrue(GeneratePass.GenerateButton().isDisplayed());
+        Assert.assertTrue(GeneratePass.GeneratedPassword().isDisplayed());
+    }
+
+    public static void ControlsAreEnabled(){
+        Assert.assertTrue(GeneratePass.MasterPasswordField().isEnabled());
+        Assert.assertTrue(GeneratePass.SiteName().isEnabled());
+        Assert.assertTrue(GeneratePass.GenerateButton().isEnabled());
+        Assert.assertTrue(GeneratePass.GeneratedPassword().isEnabled());
+    }
+
+    public static void ControlTextIsCorrect(){
+        String getMasterPasswordText = GeneratePass.MasterPasswordField().findElement(By.xpath("./../../td[1]")).getText();
+        Assert.assertEquals(getMasterPasswordText, "Your master password");
+
+        String getSiteNameText = GeneratePass.SiteName().findElement(By.xpath("./../../td[1]")).getText();
+        Assert.assertEquals(getSiteNameText, "Site name");
+
+        String getGenerateButtonText = GeneratePass.GenerateButton().getAttribute("value");
+        Assert.assertEquals(getGenerateButtonText, "Generate");
+
+        String getGeneratedPassText = GeneratePass.GeneratedPassword().findElement(By.xpath("./../../td[1]")).getText();
+        Assert.assertEquals(getGeneratedPassText, "Generated password");
     }
 }
--- allchick/My project/src/com/company/SecondTest.java
+++ allchick/My project/src/com/company/SecondTest.java
@@ -14,51 +14,108 @@ public class SecondTest {
 
     @Test
     public void pass123Name123(){
-        String generatedPass = GeneratePass.GeneratePassword("123", "123");
-        String expectedGeneratedPass = "7ttLiOwzmfYSH@1a";
-        Assert.assertEquals(expectedGeneratedPass, generatedPass);
+        GeneratePass.OpenGeneratePasswordPage();
+        GeneratePass.EnterMasterPassword("123");
+        GeneratePass.EnterSiteName("123");
+        GeneratePass.ClickGenerateButton();
+        String expectedMasterPassword = GeneratePass.GetMasterPassword();
+        Assert.assertEquals(expectedMasterPassword, "123");
+        String expectedSiteName = GeneratePass.GetSiteName();
+        Assert.assertEquals(expectedSiteName, "123");
+        String expectedGeneratedPassword = GeneratePass.GetGeneratedPassword();
+        Assert.assertEquals(expectedGeneratedPassword, "7ttLiOwzmfYSH@1a");
     }
 
     @Test
     public void passTestName321(){
-        String generatedPass = GeneratePass.GeneratePassword("Test", "321");
-        String expectedGeneratedPass = "lZ16Z2pV8mmzM@1a";
-        Assert.assertEquals(expectedGeneratedPass, generatedPass);
+        GeneratePass.OpenGeneratePasswordPage();
+        GeneratePass.EnterMasterPassword("Test");
+        GeneratePass.EnterSiteName("321");
+        GeneratePass.ClickGenerateButton();
+        String expectedMasterPassword = GeneratePass.GetMasterPassword();
+        Assert.assertEquals(expectedMasterPassword, "Test");
+        String expectedSiteName = GeneratePass.GetSiteName();
+        Assert.assertEquals(expectedSiteName, "321");
+        String expectedGeneratedPassword = GeneratePass.GetGeneratedPassword();
+        Assert.assertEquals(expectedGeneratedPassword, "lZ16Z2pV8mmzM@1a");
     }
 
     @Test
     public void passTextNameText(){
-        String generatedPass = GeneratePass.GeneratePassword("@ll4ik", "facebook.com");
-        String expectedGeneratedPass = "QG3vPAG9fYIQO@1a";
-        Assert.assertEquals(expectedGeneratedPass, generatedPass);
+        GeneratePass.OpenGeneratePasswordPage();
+        GeneratePass.EnterMasterPassword("@ll4ik");
+        GeneratePass.EnterSiteName("facebook.com");
+        GeneratePass.ClickGenerateButton();
+        String expectedMasterPassword = GeneratePass.GetMasterPassword();
+        Assert.assertEquals(expectedMasterPassword, "@ll4ik");
+        String expectedSiteName = GeneratePass.GetSiteName();
+        Assert.assertEquals(expectedSiteName, "facebook.com");
+        String expectedGeneratedPassword = GeneratePass.GetGeneratedPassword();
+        Assert.assertEquals(expectedGeneratedPassword, "QG3vPAG9fYIQO@1a");
     }
 
     @Test
     public void passNoneNameText(){
-        String generatedPass = GeneratePass.GeneratePassword("", "facebook.com");
-        String expectedGeneratedPass = "lFAosQQIiXvAR@1a";
-        Assert.assertEquals(expectedGeneratedPass, generatedPass);
+        GeneratePass.OpenGeneratePasswordPage();
+        GeneratePass.EnterMasterPassword("");
+        GeneratePass.EnterSiteName("facebook.com");
+        GeneratePass.ClickGenerateButton();
+        String expectedMasterPassword = GeneratePass.GetMasterPassword();
+        Assert.assertEquals(expectedMasterPassword, "");
+        String expectedSiteName = GeneratePass.GetSiteName();
+        Assert.assertEquals(expectedSiteName, "facebook.com");
+        String expectedGeneratedPassword = GeneratePass.GetGeneratedPassword();
+        Assert.assertEquals(expectedGeneratedPassword, "lFAosQQIiXvAR@1a");
     }
 
     @Test
     public void passTextNameNone(){
-        String generatedPass = GeneratePass.GeneratePassword("facebook.com", "");
-        String expectedGeneratedPass = "Xl4tHmP0qY15u@1a";
-        Assert.assertEquals(expectedGeneratedPass, generatedPass);
+        GeneratePass.OpenGeneratePasswordPage();
+        GeneratePass.EnterMasterPassword("facebook.com");
+        GeneratePass.EnterSiteName("");
+        GeneratePass.ClickGenerateButton();
+        String expectedMasterPassword = GeneratePass.GetMasterPassword();
+        Assert.assertEquals(expectedMasterPassword, "facebook.com");
+        String expectedSiteName = GeneratePass.GetSiteName();
+        Assert.assertEquals(expectedSiteName, "");
+        String expectedGeneratedPassword = GeneratePass.GetGeneratedPassword();
+        Assert.assertEquals(expectedGeneratedPassword, "Xl4tHmP0qY15u@1a");
     }
 
     @Test
     public void passNoneNameNone(){
-        String generatedPass = GeneratePass.GeneratePassword("", "");
-        String expectedGeneratedPass = "BaefBs8/Z/cm2@1a";
-        Assert.assertEquals(expectedGeneratedPass, generatedPass);
+        GeneratePass.OpenGeneratePasswordPage();
+        GeneratePass.EnterMasterPassword("");
+        GeneratePass.EnterSiteName("");
+        GeneratePass.ClickGenerateButton();
+        String expectedMasterPassword = GeneratePass.GetMasterPassword();
+        Assert.assertEquals(expectedMasterPassword, "");
+        String expectedSiteName = GeneratePass.GetSiteName();
+        Assert.assertEquals(expectedSiteName, "");
+        String expectedGeneratedPassword = GeneratePass.GetGeneratedPassword();
+        Assert.assertEquals(expectedGeneratedPassword, "BaefBs8/Z/cm2@1a");
     }
 
     @Test
     public void passGenPass1NameGenPass2(){
-        String generatedPass = GeneratePass.GeneratePassword("lFAosQQIiXvAR@1a", "7ttLiOwzmfYSH@1a");
-        String expectedGeneratedPass = "d4bQR1f33pW1T@1a";
-        Assert.assertEquals(expectedGeneratedPass, generatedPass);
+        GeneratePass.OpenGeneratePasswordPage();
+        GeneratePass.EnterMasterPassword("lFAosQQIiXvAR@1a");
+        GeneratePass.EnterSiteName("7ttLiOwzmfYSH@1a");
+        GeneratePass.ClickGenerateButton();
+        String expectedMasterPassword = GeneratePass.GetMasterPassword();
+        Assert.assertEquals(expectedMasterPassword, "lFAosQQIiXvAR@1a");
+        String expectedSiteName = GeneratePass.GetSiteName();
+        Assert.assertEquals(expectedSiteName, "7ttLiOwzmfYSH@1a");
+        String expectedGeneratedPassword = GeneratePass.GetGeneratedPassword();
+        Assert.assertEquals(expectedGeneratedPassword, "d4bQR1f33pW1T@1a");
+    }
+
+    @Test
+    public void smokeTest(){
+        GeneratePass.OpenGeneratePasswordPage();
+        GeneratePass.ControlsAreExist();
+        GeneratePass.ControlsAreEnabled();
+        GeneratePass.ControlTextIsCorrect();
     }
 
     @AfterClass
