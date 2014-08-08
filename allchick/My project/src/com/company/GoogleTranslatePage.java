824fd2ead6371f39433cbf092a2b26fe2ee4424e
allchick <allchick@ua.fm>
2014-08-08 21:00:40 +0300

    Added tests for GoogleTranlate

--- allchick/My project/src/com/company/GoogleTranslatePage.java
+++ allchick/My project/src/com/company/GoogleTranslatePage.java
@@ -4,10 +4,22 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import javax.security.auth.callback.LanguageCallback;
+import java.awt.*;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by user on 8/4/14.
  */
 public class GoogleTranslatePage {
+
+    public static Map<String, String> LanguageAbbreviation = new HashMap<String, String>();
+    static {
+        LanguageAbbreviation.put("English", "en");
+        LanguageAbbreviation.put("Ukrainian", "uk");
+    }
 
     public static void OpenGoogleTranslatePage(){
         ChromeWebDriver.OpenSite("https://translate.google.com/");
@@ -36,7 +48,7 @@ public class GoogleTranslatePage {
     }
 
     public static void ChooseYourNativeLanguage(String language){
-        ChromeWebDriver.webDriver.findElement(By.id("gt-tl-gms-menu")).click();
+        ChromeWebDriver.webDriver.findElement(By.id("gt-tl-gms")).click();
         String xpathForObject = "//div[@id='gt-tl-gms-menu']//div[contains(text(), '" + language + "')]";
         ChromeWebDriver.webDriver.findElement(By.xpath(xpathForObject)).click();
         CheckNativeLanguage(language);
@@ -46,6 +58,54 @@ public class GoogleTranslatePage {
         String getActiveButtonText = ChromeWebDriver.webDriver.findElement(
                 By.xpath("//div[@id = 'gt-lang-tgt']//div[contains(@class, 'jfk-button-checked')]")).getText();
         Assert.assertTrue(getActiveButtonText.contains(language));
+    }
+
+    public static void CheckUrlBeforeTranslation(String languageForTranslate, String languageNative){
+
+        String languageForTranslateAbbr = LanguageAbbreviation.get(languageForTranslate);
+        String languageNativeAbbr = LanguageAbbreviation.get(languageNative);
+        String expectedUrl ="https://translate.google.com/#" + languageForTranslateAbbr + "/" + languageNativeAbbr + "/";
+        Assert.assertTrue(ChromeWebDriver.webDriver.getCurrentUrl().contains(expectedUrl));
+    }
+
+    public static void enterTextForTranslation(String textForTranslation){
+        ControlTextForTranslation().sendKeys(textForTranslation);
+    }
+
+    public static void checkTextForTranslation(String textForTranslation){
+        Assert.assertEquals(textForTranslation, ControlTextForTranslation().getAttribute("value"));
+    }
+
+    public static void checkTranslatedText(String translatedText){
+        Assert.assertEquals(translatedText, ControlTranslatedText().getText());
+    }
+
+    private static WebElement ControlTextForTranslation(){
+        WebElement controlTextForTranslation = ChromeWebDriver.webDriver.
+                findElement(By.xpath("//textarea[@id = 'source']"));
+        return controlTextForTranslation;
+    }
+
+    private static WebElement ControlTranslatedText(){
+        while (ChromeWebDriver.webDriver.findElements(By.xpath("//span[@id = 'result_box']/span")).size() == 0);
+        WebElement controlTranslatedText = ChromeWebDriver.webDriver.
+                findElement(By.xpath("//span[@id = 'result_box']/span"));
+        return controlTranslatedText;
+    }
+
+    public static void ExecuteSetLanguagesForTranslate(String languageForTranslate, String languageNative){
+        GoogleTranslatePage.OpenGoogleTranslatePage();
+        GoogleTranslatePage.ChooseLanguageForTranslation(languageForTranslate);
+        GoogleTranslatePage.CheckLanguageForTranslation(languageForTranslate);
+        GoogleTranslatePage.ChooseYourNativeLanguage(languageNative);
+        GoogleTranslatePage.CheckNativeLanguage(languageNative);
+        GoogleTranslatePage.CheckUrlBeforeTranslation(languageForTranslate, languageNative);
+    }
+
+    public static void ExecuteTranslationText(String textForTranslate){
+        GoogleTranslatePage.enterTextForTranslation(textForTranslate);
+        GoogleTranslatePage.checkTextForTranslation(textForTranslate);
+        GoogleTranslatePage.TranslateText();
     }
 }
 
--- allchick/My project/src/com/company/TestGoogleTranslatePage.java
+++ allchick/My project/src/com/company/TestGoogleTranslatePage.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -11,10 +13,66 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class TestGoogleTranslatePage {
 
-    @Test
-    public void NewTest() {
-       GoogleTranslatePage.OpenGoogleTranslatePage();
-       GoogleTranslatePage.ChooseLanguageForTranslation("English");
-        GoogleTranslatePage.ChooseYourNativeLanguage("Ukrainian");
+    @Before
+    public void SetLanguagesForTranslate() {
+        GoogleTranslatePage.ExecuteSetLanguagesForTranslate("English", "Ukrainian");
     }
+
+    @Test
+    public void SwapLanguages() {
+        GoogleTranslatePage.CheckUrlBeforeTranslation("English", "Ukrainian");
+        GoogleTranslatePage.SwapLanguage();
+        GoogleTranslatePage.CheckUrlBeforeTranslation("Ukrainian", "English");
+    }
+
+    @Test
+    public void Translate1() {
+        GoogleTranslatePage.ExecuteTranslationText("picture");
+        GoogleTranslatePage.checkTranslatedText("фото");
+    }
+
+    @Test
+    public void Translate2() {
+        GoogleTranslatePage.ExecuteTranslationText("I am a doctor");
+        GoogleTranslatePage.checkTranslatedText("Я лікар");
+    }
+
+    @Test
+    public void Translate3() {
+        GoogleTranslatePage.ExecuteTranslationText("cat\n" + "dog");
+        GoogleTranslatePage.checkTranslatedText("кіт \n" + "собака");
+    }
+
+    @Test
+    public void Translate4() {
+        GoogleTranslatePage.ExecuteTranslationText("about box");
+        GoogleTranslatePage.checkTranslatedText("про поле");
+    }
+
+    @Test
+    public void Translate5() {
+        GoogleTranslatePage.ExecuteTranslationText("new test\n" + "new text");
+        GoogleTranslatePage.checkTranslatedText("новий тест \n" + "новий текст");
+    }
+
+    @Test
+    public void Translate6() {
+        GoogleTranslatePage.ExecuteTranslationText("NATO");
+        GoogleTranslatePage.checkTranslatedText("НАТО");
+    }
+
+    @Test
+    public void Translate7() {
+        GoogleTranslatePage.ExecuteTranslationText("ne zrozumila mova");
+        GoogleTranslatePage.checkTranslatedText("пе zrozumila мова");
+    }
+
+    @AfterClass
+    public static void closeBrowser(){
+        ChromeWebDriver.CloseWebDriver();
+    }
+
+
+
+
 }
