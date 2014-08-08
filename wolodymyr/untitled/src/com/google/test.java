f138d0fa8c3072a9662343287c2af71445385cd9
wolodymyr <wolodymyr.ch@gmail.com>
2014-08-08 21:00:38 +0300

    as;ljf;klasjf

--- wolodymyr/untitled/src/com/google/test.java
+++ wolodymyr/untitled/src/com/google/test.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 /**
  * Created by user on 8/6/14.
@@ -22,11 +23,69 @@ public class test {
 //        translator.Driwer.quit();
 //
 //    }
+@Test
+public void tranlation() throws InterruptedException {
+
+    translator.selectLanguageS("gt-sl-gms", "//div[@id='gt-sl-gms-menu']//div[contains(text(), 'англ')]");
+    translator.selectLanguageS("gt-tl-gms", "//div[@id='gt-tl-gms-menu']//div[contains(text(), 'рос')]");
+    translator.enterWord("box");
+    translator.Tranlate();
+    Assert.assertEquals(translator.getTword(), "коробка");
+    }
     @Test
-    public void tranlation() {
-        translator.selectLanguageS("gt-sl-gms", "//div[text()='іврит']");
-        translator.selectLanguageS( "gt-ll-gms", "//div[text()='каннада']");
+    public void isButon1(){
+       Assert.assertTrue(translator.isButonAvbyxpath("//div[@id='gt-sl-sugg']/div[2]"));
+    }
+    @Test
+    public void isButtonS2(){
+        Assert.assertTrue(translator.isButonAvbyxpath("//div[@id='gt-sl-sugg']/div[1]"));
+    }
+    @Test
+    public void isButtonS3(){
+        Assert.assertTrue(translator.isButonAvbyxpath("//div[@id='gt-sl-sugg']/div[3]"));
+    }
+    @Test
+    public void isButtonS4(){
+        Assert.assertTrue(translator.isButonAvbyxpath("//div[@id='gt-sl-sugg']/div[5]"));
+    }
+    @Test
+    public void isButonT1(){
+        Assert.assertTrue(translator.isButonAvbyxpath("//div[@id='gt-tl-sugg']/div[1]"));
+    }
+    @Test
+    public void isButtonT2(){
+        Assert.assertTrue(translator.isButonAvbyxpath("//div[@id='gt-tl-sugg']/div[2]"));
+    }
+    @Test
+    public void isButtonT3(){
+        Assert.assertTrue(translator.isButonAvbyxpath("//div[@id='gt-tl-sugg']/div[3]"));
+    }
+    @Test
+    public void isButtonTr(){
+        Assert.assertTrue(translator.isButonAvbyid("gt-submit"));
+    }
+    public void isButtonCh(){
+        Assert.assertTrue(translator.isButonAvbyid("gt-swap"));
+    }
+    @Test
+    public void isMenuS(){
+        Assert.assertTrue(translator.isButonAvbyid("gt-sl-gms"));
+    }
+    @Test
+    public void isMenuT(){
+        Assert.assertTrue(translator.isButonAvbyid("gt-tl-gms"));
+    }
+    @Test
+    public void isTextS(){
+        Assert.assertTrue(translator.isButonAvbyid("source"));
+    }
+    @Test
+    public void isSelected(){
+        translator.isButActive("//div[@id='gt-sl-sugg']/div[1]");
 
     }
+
+
+
 
 }
--- wolodymyr/untitled/src/com/google/translator.java
+++ wolodymyr/untitled/src/com/google/translator.java
@@ -21,10 +21,33 @@ public class translator {
         cw.get("https://translate.google.com.ua/");
         Driwer=cw;
     }
-    public static void selectLanguageS (String id, String xpath){
+    public static void selectLanguageS (String id, String xpath) {
         Driwer.findElement(By.id(id)).click();
         Driwer.findElement(By.xpath(xpath)).click();
-
+    }
+    public static boolean isButonAvbyid (String idButton) {
+        return Driwer.findElement(By.id(idButton)).isEnabled();
+    }
+    public static boolean isButonAvbyxpath (String idButton) {
+        return Driwer.findElement(By.xpath(idButton)).isEnabled();
+    }
+    public static String getTword(){
+        if (Driwer.findElement(By.xpath("//span[@class='hps'])")).isEnabled()){
+            return Driwer.findElement(By.xpath("//span[@class='hps'])")).getText();
+        } else {
+            return "";}
+    }
+    public static void enterWord(String word){
+        Driwer.findElement(By.id("source")).sendKeys(word);
+    }
+    public static void ClickByttonbyID (){
+        Driwer.findElement(By.id("gt-submit")).click();
+    }
+    public static String getAriaPressed(String xpath){
+        return  Driwer.findElement(By.xpath(xpath)).getAttribute("aria-pressed");
+    }
+    public static void ClickButtonbyXpath(String xpath){
+        Driwer.findElement(By.xpath(xpath)).click();
     }
 
 
